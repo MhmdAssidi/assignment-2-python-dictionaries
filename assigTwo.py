f5629db89df4451a32dc039b5ba01536620ae8e9
Mohammad Assidi <mhmdassidi2002@egmail.com>
2025-03-01 00:37:57 +0200

    Group dictionary keys by their values

--- assigTwo.py
+++ assigTwo.py
@@ -1,17 +1,19 @@
-# def mergedFunction(dict1,dict2):
-#     mergedDict={}
+def mergedFunction(dict1,dict2):
+    mergedDict={}
     
-#     y=list(dict2.keys())
-#     for i in dict1:
-#         if i in dict2:
-#             mergedDict[i]=dict2[i]
-#         else:
-#             mergedDict[i]=dict1[i]
-#     for j in dict2: #add the keys that are in second dictionary
-#         if j not in mergedDict:
-#             mergedDict[j]=dict2[j]
+    y=list(dict2.keys())
+    for i in dict1:
+        if i in dict2:
+            mergedDict[i]=dict2[i]
+        else:
+            mergedDict[i]=dict1[i]
+    for j in dict2: #add the keys that are in second dictionary
+        if j not in mergedDict:
+            mergedDict[j]=dict2[j]
 
-#     return mergedDict
+    return mergedDict
+
+# b:
 def totNumEmp(dict):
     count=0
     for i in dict.values():
@@ -22,28 +24,28 @@ def totNumEmp(dict):
 
 
 
-# dict1 = {
-#     "a": 1,
-#     "b": 2,
-#     "c": 3}
-# dict2 = {
-#     "b": 10, 
-#     "d": 4
-#     }
-# print(mergedFunction(dict1,dict2))
+dict1 = {
+    "a": 1,
+    "b": 2,
+    "c": 3}
+dict2 = {
+    "b": 10, 
+    "d": 4
+    }
+print(mergedFunction(dict1,dict2))
 
-# sentence=input("enter a sentence to print how many times each word repeated: ")
-# words =sentence.split()
-# # print(words)
-# count=0
-# wordAppearDict={}
-# for word in words:
-#     for w in words:
-#         if word==w:
-#             count=count+1
-#     wordAppearDict[word]=count
-#     count=0
-# print (wordAppearDict)            
+sentence=input("enter a sentence to print how many times each word repeated: ")
+words =sentence.split()
+# print(words)
+count=0
+wordAppearDict={}
+for word in words:
+    for w in words:
+        if word==w:
+            count=count+1
+    wordAppearDict[word]=count
+    count=0
+print (wordAppearDict)            
 
 # ex3:
 company_employees = {
@@ -64,5 +66,19 @@ company_employees["Engineering"]["David"]={"age":27,"role":"Data Scientist"}
 
 print("after adding the employee David, the dictionary is: ",company_employees)
 
+# b:
 print("after adding david the number of employees in all the company is: ",totNumEmp(company_employees))
+
+# c:
+dict={"Alice": 10, "Bob": 20, "Charlie": 10, "David": 30}
+
+tempDict={}
+for i in dict.values():
+    tempDict[i]=[]
+    for key,j in dict.items():
+        if i==j:
+            tempDict[i].append(key)
+print(tempDict)
+
+
 
