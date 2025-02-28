3f32fb8f40e96417d98adf893f88b5d41a855518
Mohammad Assidi <mhmdassidi2002@egmail.com>
2025-02-28 22:14:29 +0200

    adding the employee David to the company dictionary at the engineering department key

--- assigTwo.py
+++ assigTwo.py
@@ -1,41 +1,41 @@
-def mergedFunction(dict1,dict2):
-    mergedDict={}
+# def mergedFunction(dict1,dict2):
+#     mergedDict={}
     
-    y=list(dict2.keys())
-    for i in dict1:
-        if i in dict2:
-            mergedDict[i]=dict2[i]
-        else:
-            mergedDict[i]=dict1[i]
-    for j in dict2: #add the keys that are in second dictionary
-        if j not in mergedDict:
-            mergedDict[j]=dict2[j]
+#     y=list(dict2.keys())
+#     for i in dict1:
+#         if i in dict2:
+#             mergedDict[i]=dict2[i]
+#         else:
+#             mergedDict[i]=dict1[i]
+#     for j in dict2: #add the keys that are in second dictionary
+#         if j not in mergedDict:
+#             mergedDict[j]=dict2[j]
 
-    return mergedDict
+#     return mergedDict
 
 
-dict1 = {
-    "a": 1,
-    "b": 2,
-    "c": 3}
-dict2 = {
-    "b": 10, 
-    "d": 4
-    }
-print(mergedFunction(dict1,dict2))
+# dict1 = {
+#     "a": 1,
+#     "b": 2,
+#     "c": 3}
+# dict2 = {
+#     "b": 10, 
+#     "d": 4
+#     }
+# print(mergedFunction(dict1,dict2))
 
-sentence=input("enter a sentence to print how many times each word repeated: ")
-words =sentence.split()
-# print(words)
-count=0
-wordAppearDict={}
-for word in words:
-    for w in words:
-        if word==w:
-            count=count+1
-    wordAppearDict[word]=count
-    count=0
-print (wordAppearDict)            
+# sentence=input("enter a sentence to print how many times each word repeated: ")
+# words =sentence.split()
+# # print(words)
+# count=0
+# wordAppearDict={}
+# for word in words:
+#     for w in words:
+#         if word==w:
+#             count=count+1
+#     wordAppearDict[word]=count
+#     count=0
+# print (wordAppearDict)            
 
 # ex3:
 company_employees = {
@@ -50,4 +50,6 @@ company_employees = {
 #a:
 print(company_employees)
 
+company_employees["Engineering"]["David"]={"age":27,"role":"Data Scientist"}
+print("after adding the employee David, the dictionary is: ",company_employees)
 
